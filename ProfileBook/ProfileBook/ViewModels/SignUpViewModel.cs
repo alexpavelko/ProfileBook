e4f5435b8b5b08dddef9333f371f33e56b18e061
alexpavelko <pavelkobond@gmail.com>
2021-04-15 02:25:22 +0300

    Add navigation to SignInView

--- ProfileBook/ProfileBook/ViewModels/SignUpViewModel.cs
+++ ProfileBook/ProfileBook/ViewModels/SignUpViewModel.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+using ProfileBook.Views;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace ProfileBook.ViewModels
 {
-    public class SignUpViewModel
+    public class SignUpViewModel : BindableBase
     {
+        public ICommand BackCommand => new Command(OnBack);
+
+        private INavigationService _navigationService;
+
+        private void OnBack()
+        {
+            _navigationService.NavigateAsync(nameof(SignInView));
+        }
+
+        public SignUpViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
     }
 }
