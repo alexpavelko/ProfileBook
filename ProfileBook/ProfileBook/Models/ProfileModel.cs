a823ffaad1e50e181fd014efdeb2f3d01d256ad3
alexpavelko <pavelkobond@gmail.com>
2021-04-16 02:36:20 +0300

    Add ProfileModel

---
+++ ProfileBook/ProfileBook/Models/ProfileModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProfileBook.Models
+{
+    class ProfileModel
+    {
+        public int Id { get; set; }        
+        public string NickName { get; set; }
+        public string Name { get; set; }
+        public string CreationTime { get; set; }
+        public string ProfileImage { get; set; }
+    }
+}
