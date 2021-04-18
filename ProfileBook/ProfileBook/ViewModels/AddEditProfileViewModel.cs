9e199c3a2c354fcc759524a76058b561001448ce
alexpavelko <pavelkobond@gmail.com>
2021-04-18 20:49:59 +0300

    AddEditProfileViewModel : Take Photo to gallery and set as main user photo

--- ProfileBook/ProfileBook/ViewModels/AddEditProfileViewModel.cs
+++ ProfileBook/ProfileBook/ViewModels/AddEditProfileViewModel.cs
@@ -114,7 +114,29 @@ namespace ProfileBook.ViewModels
             }
         }
 
-       
+        private async void TakePhotoAsync()
+        {
+            try
+            {
+                var photo = await MediaPicker.CapturePhotoAsync(new MediaPickerOptions
+                {
+                    Title = $"xamarin.{DateTime.Now.ToString("dd.MM.yyyy_hh.mm.ss")}.png"
+                });
+
+
+                var newFile = Path.Combine(FileSystem.AppDataDirectory, photo.FileName);
+                using (var stream = await photo.OpenReadAsync())
+                using (var newStream = File.OpenWrite(newFile))
+                    await stream.CopyToAsync(newStream);
+
+
+                ProfileImage = photo.FullPath;
+            }
+            catch (Exception ex)
+            {
+                await UserDialogs.Instance.AlertAsync(ex.Message, "Error", "OK");
+            }
+        }
 
         private bool Validate()
         {
