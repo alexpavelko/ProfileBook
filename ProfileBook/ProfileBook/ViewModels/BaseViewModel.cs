4c1d5d5d8c699287f640cabe5c429275ede4008d
alexpavelko <pavelkobond@gmail.com>
2021-04-23 15:27:35 +0300

    Add BaseViewModel

---
+++ ProfileBook/ProfileBook/ViewModels/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace ProfileBook.ViewModels
+{
+    public class BaseViewModel : BindableBase, IInitialize, INavigationAware
+    {
+        protected INavigationService NavigationService { get; private set; }
+        public BaseViewModel(INavigationService navigationService)
+        {
+           NavigationService = navigationService;
+        }
+        public void Initialize(INavigationParameters parameters)
+        {
+           
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+           
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
+           
+        }
+    }
+}
