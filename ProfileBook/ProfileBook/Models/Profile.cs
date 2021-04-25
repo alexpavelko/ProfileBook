76618bd8dda114b10f643c4d0126c21c0107ea72
alexpavelko <pavelkobond@gmail.com>
2021-04-25 23:56:47 +0300

    Set defaut model

--- ProfileBook/ProfileBook/Models/Profile.cs
+++ ProfileBook/ProfileBook/Models/Profile.cs
@@ -1,4 +1,6 @@
 ﻿using SQLite;
+using System;
+using Xamarin.Essentials;
 
 namespace ProfileBook.Models
 {
@@ -6,12 +8,19 @@ namespace ProfileBook.Models
     public class Profile : IEntityBase
     {
         [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }        
+        public int Id { get; set; }
         public string NickName { get; set; }
         public string Name { get; set; }
         public string CreationTime { get; set; }
         public string ProfileImage { get; set; }
         public string Description { get; set; }
         public int UserId { get; set; }
+
+        public Profile()
+        {
+            this.ProfileImage = "user_preson.png";
+            this.CreationTime = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+            this.UserId = Preferences.Get($"{nameof(UserId)}", 0);
+        }
     }
 }
