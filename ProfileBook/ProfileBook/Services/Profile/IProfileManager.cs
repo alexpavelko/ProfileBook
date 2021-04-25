6899e91255e0217f0c444fc23bcc6fe9862f6454
alexpavelko <pavelkobond@gmail.com>
2021-04-25 23:59:04 +0300

    Add ProfileManager

---
+++ ProfileBook/ProfileBook/Services/Profile/IProfileManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileBook.Services.Profile
+{
+    public interface IProfileManager
+    {
+        Task<List<Models.Profile>> GetProfiles(int user_id);
+        Task SaveProfile(Models.Profile profile);
+        Task RemoveProfile(Models.Profile profile);
+    }
+}
---
+++ ProfileBook/ProfileBook/Services/Profile/ProfileManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+using ProfileBook.Services.Repository;
+
+namespace ProfileBook.Services.Profile
+{
+    public class ProfileManager : IProfileManager
+    {
+        private IRepository _repository;
+        public ProfileManager(IRepository repository)
+        {
+            this._repository = repository;
+        }
+        public async Task<List<Models.Profile>> GetProfiles(int user_id)
+        {
+            string sqlCommand = $"$SELECT * FROM Profiles WHERE UserId = {user_id}";
+            return await _repository.GetAllWithQueryAsync<Models.Profile>(sqlCommand);
+        }
+
+        public async Task RemoveProfile(Models.Profile profile)
+        {
+            if (File.Exists(profile.ProfileImage))
+                File.Delete(profile.ProfileImage);
+
+            await _repository.DeleteAsync(profile);
+        }
+
+        public async Task SaveProfile(Models.Profile profile)
+        {
+            await _repository.AddOrUpdateAsync(profile);
+        }
+    }
+}
