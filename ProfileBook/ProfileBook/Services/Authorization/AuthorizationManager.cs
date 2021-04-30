a1ecf46fa074086959b24df86283f2847a77185c
alexpavelko <pavelkobond@gmail.com>
2021-04-30 13:45:52 +0300

    Add AuthorizationManager

---
+++ ProfileBook/ProfileBook/Services/Authorization/AuthorizationManager.cs
@@ -0,0 +1,45 @@
+﻿using ProfileBook.Models;
+using ProfileBook.Services.Repository;
+using ProfileBook.Services.Settings;
+using System.Threading.Tasks;
+
+namespace ProfileBook.Services.Authorization
+{
+    public class AuthorizationManager : IAuthorizationManager
+    {
+        private IRepository _repository;
+        private ISettingsManager _settingsManager;
+        public AuthorizationManager(IRepository repository, ISettingsManager settingsManager)
+        {
+            _repository = repository;
+            _settingsManager = settingsManager;
+        }
+        public async Task<bool> SignIn(string login, string password)
+        {
+            string sqlCommand = $"SELECT * FROM Users WHERE Login='{login}' AND Password='{password}'";
+            User user = await _repository.FindWithCommand<User>(sqlCommand);
+            if(user != null)
+            {
+                _settingsManager.UserId = user.Id;
+                return true;
+            }
+            return false;
+        }
+        public async Task<bool> RegisterUser(string login, string password)
+        {
+            string sqlCommand = $"SELECT * FROM Users WHERE Login='{login}' AND Password='{password}'";
+
+            User user = await _repository.FindWithCommand<User>(sqlCommand);
+            if (user != null)
+                return false;
+
+            User new_user = new User
+            {
+                Login = login,
+                Password = password
+            };
+            await _repository.AddAsync<User>(new_user);
+            return true;
+        }
+    }
+}
---
+++ ProfileBook/ProfileBook/Services/Authorization/IAuthorizationManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace ProfileBook.Services.Authorization
+{
+    public interface IAuthorizationManager
+    {
+        Task<bool> RegisterUser(string login, string password);
+        Task<bool> SignIn(string login, string password);
+    }
+}
